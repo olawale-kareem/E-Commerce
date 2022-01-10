91fcb836fcdffd47ea130483636d8cfe25479310
olawale-kareem <olawale.kareem@decagon.dev>
2022-01-10 01:23:53 +0100

    charting implementations get all category price data

--- Charts/views.py
+++ Charts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import status, response
-from ..expenses.models import Expense
+from expenses.models import Expense
 import datetime
 
 
@@ -20,11 +20,13 @@ class ExpensesSummaryStats(APIView):
         return {'amount': str(amount)}
 
     def get(self, request):
+
         todays_date = datetime.date.today()
         ayear_ago = todays_date - datetime.timedelta(days=30*12)
         expenses = Expense.objects.filter(
             owner=request.user, date__gte=ayear_ago, date__lte=todays_date
         )
+
         final = {}
         categories = list(set(map(self.get_category, expenses)))
 
@@ -33,4 +35,4 @@ class ExpensesSummaryStats(APIView):
                 final[category] = self.get_amount_for_category(
                     expenses, category)
 
-        return response({'category_data': final}, status=status.HTTP_200_OK)
+        return response.Response({'category_data': final}, status=status.HTTP_200_OK)

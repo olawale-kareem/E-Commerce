e7840a7cdeee743aa8418dfd2b29064f14a8d786
olawale-kareem <olawale.kareem@decagon.dev>
2022-02-17 04:10:34 +0100

    requirements changes

--- authentication/views.py
+++ authentication/views.py
@@ -12,11 +12,9 @@ import jwt                            # comes withthe simple jwt install
 from django.conf import settings
 
 # serializers
-from .serializers import (RegisterSerializer, EmailVerificationSerializer, LoginSerializer, 
-                            ResetPasswordEmailRequestSerializer,SetNewPasswordSerializer, 
-                            CheckPasswordSerializer, LogoutSerializer)
-
-
+from .serializers import (RegisterSerializer, EmailVerificationSerializer, LoginSerializer,
+                          ResetPasswordEmailRequestSerializer, SetNewPasswordSerializer,
+                          CheckPasswordSerializer, LogoutSerializer)
 
 # swagger
 from drf_yasg.utils import swagger_auto_schema
@@ -32,11 +30,10 @@ from .utils import Util
 from rest_framework.exceptions import AuthenticationFailed
 
 # logout views
-from rest_framework import  permissions
+from rest_framework import permissions
 
 # renderers
 from .renderers import UserRender
-
 
 
 ####
@@ -44,6 +41,7 @@ from .renderers import UserRender
 class RegisterView(generics.GenericAPIView):
     serializer_class = RegisterSerializer
     renderer_classes = (UserRender,)
+
     def post(self, request):
         user = request.data
         serializer = self.serializer_class(data=user)
@@ -67,18 +65,21 @@ class RegisterView(generics.GenericAPIView):
 
         return Response(user_data, status=status.HTTP_201_CREATED)
 
+
 class VerifyEmail(views.APIView):
     serializer_class = EmailVerificationSerializer
     token_param_config = openapi.Parameter('token', in_=openapi.IN_QUERY,
-                                             description='description', type=openapi.TYPE_STRING)
+                                           description='description', type=openapi.TYPE_STRING)
+
     @swagger_auto_schema(manual_parameters=[token_param_config])
     def get(self, request):
         token = request.GET.get('token')
-        
+
         try:
-            payload = jwt.decode(token, settings.SECRET_KEY, algorithms='HS256')
+            payload = jwt.decode(
+                token, settings.SECRET_KEY, algorithms='HS256')
             user = User.objects.get(id=payload['user_id'])
-            if  not user.is_verified:
+            if not user.is_verified:
                 user.is_verified = True
                 user.save()
                 data = {"email": "email is succefully registered"}
@@ -92,12 +93,15 @@ class VerifyEmail(views.APIView):
             data = {"error": "Invalid token"}
             return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
+
 class LoginAPIView(generics.GenericAPIView):
     serializer_class = LoginSerializer
-    def post(self,request):
+
+    def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        return Response(serializer.data, status= status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class LogoutAPIView(generics.GenericAPIView):
     serializer_class = LogoutSerializer
@@ -116,43 +120,48 @@ class LogoutAPIView(generics.GenericAPIView):
 class RequestPasswordResetEmail(generics.GenericAPIView):
     serializer_class = ResetPasswordEmailRequestSerializer
 
-    def post(self,request):
+    def post(self, request):
         serializer = self.serializer_class(data=request.data)
-        email = request.data['email']    
+        email = request.data['email']
         if User.objects.filter(email=email).exists():
-            user = User.objects.get(email=email) 
+            user = User.objects.get(email=email)
             uidb64 = urlsafe_base64_encode(smart_bytes(user.id))
-            token =PasswordResetTokenGenerator().make_token(user)
+            token = PasswordResetTokenGenerator().make_token(user)
             # mail body set up
             current_site = get_current_site(request=request).domain
-            relative_link = reverse('password-reset-confirm',kwargs={'uidb64':uidb64,'token':token})
+            relative_link = reverse(
+                'password-reset-confirm', kwargs={'uidb64': uidb64, 'token': token})
             absurl = "http://" + current_site + relative_link
             email_body = "Hello, \n Use the link to reset your password \n" + absurl
             data = {'email_body': email_body, 'to_email': user.email,
                     'email_subject': 'Reset your password'}
-            Util.send_email(data)               
-            return Response({'success':'we have sent you a link to reset the password'}, status=status.HTTP_200_OK)
+            Util.send_email(data)
+            return Response({'success': 'we have sent you a link to reset the password'}, status=status.HTTP_200_OK)
         else:
-            return Response({'Failed':'Incorrect Email supplied'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'Failed': 'Incorrect Email supplied'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class PasswordTokenCheckAPI(generics.GenericAPIView):
     serializer_class = CheckPasswordSerializer
+
     def get(self, request, uidb64, token):
         try:
             id = smart_str(urlsafe_base64_decode(uidb64))
             user = User.objects.get(id=id)
 
             if not PasswordResetTokenGenerator().check_token(user, token):
-                return Response({'error':'Token not valid please request for a new one'})
+                return Response({'error': 'Token not valid please request for a new one'})
 
-            return Response({'success':True, 'message': 'Credentials Valid', 'uidb64':uidb64, 'token': token}, status=status.HTTP_200_OK)
+            return Response({'success': True, 'message': 'Credentials Valid', 'uidb64': uidb64, 'token': token}, status=status.HTTP_200_OK)
 
         except DjangoUnicodeDecodeError as identifier:
-                return Response({'error':'invalid token'})
+            return Response({'error': 'invalid token'})
+
 
 class SetNewPasswordAPIView(generics.GenericAPIView):
     serializer_class = SetNewPasswordSerializer
     permission_classes = (permissions.AllowAny,)
+
     def post(self, request, uidb64):
         user_data = request.data
         try:
@@ -165,11 +174,12 @@ class SetNewPasswordAPIView(generics.GenericAPIView):
             confirm_password = user_data.get('confirm_password')
             print(password, confirm_password)
             if password != confirm_password:
-                return Response({'failed':f'Password is not the same'}, status=status.HTTP_200_OK)
+                return Response({'failed': f'Password is not the same'}, status=status.HTTP_200_OK)
             user.set_password(password)
             user.save()
-            return Response({'success':f'Password reset succesful'}, status=status.HTTP_200_OK)
-      
+            return Response({'success': f'Password reset succesful'}, status=status.HTTP_200_OK)
+
+
 class LogoutAPIView(generics.GenericAPIView):
     serializer_class = LogoutSerializer
 
@@ -184,18 +194,16 @@ class LogoutAPIView(generics.GenericAPIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-
-
 #######
 # class RequestPasswordResetEmail(generics.GenericAPIView):
-    
+
 #     serializer_class = ResetPasswordEmailRequestSerializer
 
 #     def post(self,request):
 #         serializer = self.serializer_class(data=request.data)
-#         email = request.data['email']    
+#         email = request.data['email']
 #         if User.objects.filter(email=email).exists():
-#             user = User.objects.get(email=email) 
+#             user = User.objects.get(email=email)
 #             uidb64 = urlsafe_base64_encode(smart_bytes(user.id))
 
 #             current_site = get_current_site(request=request).domain
@@ -208,16 +216,3 @@ class LogoutAPIView(generics.GenericAPIView):
 #             return Response({'success':'we have sent you a link to reset the password'}, status=status.HTTP_200_OK)
 #         else:
 #              return Response({'Failed':'Incorrect Email supplied'}, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
-  
-
-        
-
-
-
-
-
-        
